206fad97b03e24f3635a0a37910a6dc42599f7e7
Pedro Henrique A. Herzog <pedrohaherzog2005@gmail.com>
2026-06-20 21:01:37 -0300

    feat: adicionar páginas de avaliação de projetos e listagem de projetos, além de css adicionado a página principal.

--- front/src/app/page.js
+++ front/src/app/page.js
@@ -1,7 +1,36 @@
+import Link from "next/link";
+import "./globals.css";
+
 export default function Home() {
   return (
-    <>
-    <div>Home</div>
-    </>
+    <main className="container">
+      <h1 className="title">Bem-vindo ao PIE Manager</h1>
+      <p className="subtitle">
+        Selecione abaixo a operação que deseja realizar no sistema:
+      </p>
+
+      <div className="grid">
+        <Link href="/projetos" className="card">
+          <h2 style={{ color: "var(--primary-color)", marginBottom: "0.5rem" }}>
+            Ver Projetos
+          </h2>
+          <p>Listar e gerenciar os grupos existentes</p>
+        </Link>
+
+        <Link href="/grupo" className="card">
+          <h2 style={{ color: "#16a34a", marginBottom: "0.5rem" }}>
+            Novo Grupo
+          </h2>
+          <p>Cadastrar um novo Projeto Integrador</p>
+        </Link>
+
+        <Link href="/avaliacoes" className="card">
+          <h2 style={{ color: "#9333ea", marginBottom: "0.5rem" }}>
+            Avaliações
+          </h2>
+          <p>Lançar notas e comentários aos projetos</p>
+        </Link>
+      </div>
+    </main>
   );
 }
